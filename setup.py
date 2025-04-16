57dbea066553b4cd5837a2d655e215704943ef20
Preet S <preet@weirdlabs.in>
2025-04-16 21:48:28 +0530

    Version Changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="utm-tracker-django",
-    version="1.1.0",
+    version="1.1.1",
     description="Middleware to track UTM parameters in Django",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
